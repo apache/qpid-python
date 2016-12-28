apache/qpid-python

commit 7e0e81b175114a0c965ffa55bb7c152b93a970a8

QPID-7588: [Python Test Suite] [0-8..0-91]  Allow message receive timeouts to be configured from the command line (-D) to accomodate slower CI environments

qpid/testlib.py
@@ -46,6 +46,10 @@ class TestBase(unittest.TestCase):
     ...) which are wrappers for the Channel functions that note
     resources to clean up later.
     """
+    DEFAULT_USERNAME = "guest"
+    DEFAULT_PASSWORD = "guest"
+    DEFAULT_PORT = 5672
+    DEFAULT_PORT_TLS = 5671
 
     def configure(self, config):
         self.config = config
@@ -73,12 +77,20 @@ class TestBase(unittest.TestCase):
 
         self.client.close()
 
+    def recv_timeout(self):
+        """Timeout used when a message is anticipated."""
+        return float(self.config.defines.get("qpid.recv_timeout", "1"))
+
+    def recv_timeout_negative(self):
+        """Timeout used when a message is NOT expected."""
+        return float(self.config.defines.get("qpid.recv_timeout_negative", "0.5"))
+
     def connect(self, host=None, port=None, user=None, password=None, tune_params=None, client_properties=None, channel_options=None):
         """Create a new connction, return the Client object"""
         host = host or self.config.broker.host
-        port = port or self.config.broker.port or 5672
-        user = user or self.config.broker.user or "guest"
-        password = password or self.config.broker.password or "guest"
+        port = port or self.config.broker.port or self.DEFAULT_PORT
+        user = user or self.config.broker.user or self.DEFAULT_USERNAME
+        password = password or self.config.broker.password or self.DEFAULT_PASSWORD
         client = qpid.client.Client(host, port)
         try:
           client.start(username = user, password=password, tune_params=tune_params, client_properties=client_properties, channel_options=channel_options)
@@ -210,17 +222,17 @@ class TestBase010(unittest.TestCase):
     def connect(self, host=None, port=None):
         url = self.broker
         if url.scheme == URL.AMQPS:
-            default_port = 5671
+            default_port = self.DEFAULT_PORT_TLS
         else:
-            default_port = 5672
+            default_port = self.DEFAULT_PORT
         try:
             sock = connect(host or url.host, port or url.port or default_port)
         except socket.error, e:
             raise Skipped(e)
         if url.scheme == URL.AMQPS:
             sock = ssl(sock)
-        conn = Connection(sock, username=url.user or "guest",
-                          password=url.password or "guest")
+        conn = Connection(sock, username=url.user or self.DEFAULT_USERNAME,
+                          password=url.password or self.DEFAULT_PASSWORD)
         try:
             conn.start(timeout=10)
         except VersionError, e:

qpid_tests/broker_0_8/testlib.py
@@ -42,7 +42,7 @@ class TestBaseTest(TestBase):
         q = self.consume("empty")
         self.assertEmpty(q)
         try:
-            q.get(timeout=1)
+            q.get(timeout=self.recv_timeout())
             self.fail("Queue is not empty.")
         except Empty: None              # Ignore
 

qpid_tests/broker_0_8/basic.py
@@ -43,10 +43,10 @@ class BasicTests(TestBase):
         #check the queues of the two consumers
         excluded = self.client.queue("local_excluded")
         included = self.client.queue("local_included")
-        msg = included.get(timeout=1)
+        msg = included.get(timeout=self.recv_timeout())
         self.assertEqual("consume_no_local", msg.content.body)
         try:
-            excluded.get(timeout=1) 
+            excluded.get(timeout=self.recv_timeout())
             self.fail("Received locally published message though no_local=true")
         except Empty: None
 
@@ -113,7 +113,7 @@ class BasicTests(TestBase):
         queue = durable_subscription_client.queue(subscription.consumer_tag)
 
         # consume and verify message content
-        msg = queue.get(timeout=1)
+        msg = queue.get(timeout=self.recv_timeout())
         self.assertEqual(test_message, msg.content.body)
         consumerchannel.basic_ack(delivery_tag=msg.delivery_tag)
       finally:
@@ -172,14 +172,14 @@ class BasicTests(TestBase):
         channel.basic_publish(routing_key="test-queue-4", content=Content("One"))
 
         myqueue = self.client.queue("my-consumer")
-        msg = myqueue.get(timeout=1)
+        msg = myqueue.get(timeout=self.recv_timeout())
         self.assertEqual("One", msg.content.body)
 
         #cancel should stop messages being delivered
         channel.basic_cancel(consumer_tag="my-consumer")
         channel.basic_publish(routing_key="test-queue-4", content=Content("Two"))
         try:
-            msg = myqueue.get(timeout=1) 
+            msg = myqueue.get(timeout=self.recv_timeout())
             self.fail("Got message after cancellation: " + msg)
         except Empty: None
 
@@ -204,11 +204,11 @@ class BasicTests(TestBase):
         channel.basic_publish(routing_key="test-ack-queue", content=Content("Four"))
         channel.basic_publish(routing_key="test-ack-queue", content=Content("Five"))
                 
-        msg1 = queue.get(timeout=1)
-        msg2 = queue.get(timeout=1)
-        msg3 = queue.get(timeout=1)
-        msg4 = queue.get(timeout=1)
-        msg5 = queue.get(timeout=1)
+        msg1 = queue.get(timeout=self.recv_timeout())
+        msg2 = queue.get(timeout=self.recv_timeout())
+        msg3 = queue.get(timeout=self.recv_timeout())
+        msg4 = queue.get(timeout=self.recv_timeout())
+        msg5 = queue.get(timeout=self.recv_timeout())
         
         self.assertEqual("One", msg1.content.body)
         self.assertEqual("Two", msg2.content.body)
@@ -221,14 +221,14 @@ class BasicTests(TestBase):
 
         channel.basic_recover(requeue=False)
         
-        msg3b = queue.get(timeout=1)
-        msg5b = queue.get(timeout=1)
+        msg3b = queue.get(timeout=self.recv_timeout())
+        msg5b = queue.get(timeout=self.recv_timeout())
         
         self.assertEqual("Three", msg3b.content.body)
         self.assertEqual("Five", msg5b.content.body)
 
         try:
-            extra = queue.get(timeout=1)
+            extra = queue.get(timeout=self.recv_timeout())
             self.fail("Got unexpected message: " + extra.content.body)
         except Empty: None
 
@@ -248,11 +248,11 @@ class BasicTests(TestBase):
         channel.basic_publish(routing_key="test-requeue", content=Content("Four"))
         channel.basic_publish(routing_key="test-requeue", content=Content("Five"))
                 
-        msg1 = queue.get(timeout=1)
-        msg2 = queue.get(timeout=1)
-        msg3 = queue.get(timeout=1)
-        msg4 = queue.get(timeout=1)
-        msg5 = queue.get(timeout=1)
+        msg1 = queue.get(timeout=self.recv_timeout())
+        msg2 = queue.get(timeout=self.recv_timeout())
+        msg3 = queue.get(timeout=self.recv_timeout())
+        msg4 = queue.get(timeout=self.recv_timeout())
+        msg5 = queue.get(timeout=self.recv_timeout())
         
         self.assertEqual("One", msg1.content.body)
         self.assertEqual("Two", msg2.content.body)
@@ -270,8 +270,8 @@ class BasicTests(TestBase):
         subscription2 = channel.basic_consume(queue="test-requeue")
         queue2 = self.client.queue(subscription2.consumer_tag)
         
-        msg3b = queue2.get(timeout=1)
-        msg5b = queue2.get(timeout=1)
+        msg3b = queue2.get(timeout=self.recv_timeout())
+        msg5b = queue2.get(timeout=self.recv_timeout())
         
         self.assertEqual("Three", msg3b.content.body)
         self.assertEqual("Five", msg5b.content.body)
@@ -280,11 +280,11 @@ class BasicTests(TestBase):
         self.assertEqual(True, msg5b.redelivered)
 
         try:
-            extra = queue2.get(timeout=1)
+            extra = queue2.get(timeout=self.recv_timeout())
             self.fail("Got unexpected message in second queue: " + extra.content.body)
         except Empty: None
         try:
-            extra = queue.get(timeout=1)
+            extra = queue.get(timeout=self.recv_timeout())
             self.fail("Got unexpected message in original queue: " + extra.content.body)
         except Empty: None
         
@@ -308,10 +308,10 @@ class BasicTests(TestBase):
 
         #only 5 messages should have been delivered:
         for i in range(1, 6):
-            msg = queue.get(timeout=1)
+            msg = queue.get(timeout=self.recv_timeout())
             self.assertEqual("Message %d" % i, msg.content.body)
         try:
-            extra = queue.get(timeout=1)
+            extra = queue.get(timeout=self.recv_timeout())
             self.fail("Got unexpected 6th message in original queue: " + extra.content.body)
         except Empty: None
 
@@ -319,13 +319,13 @@ class BasicTests(TestBase):
         channel.basic_ack(delivery_tag=msg.delivery_tag, multiple=True)
 
         for i in range(6, 11):
-            msg = queue.get(timeout=1)
+            msg = queue.get(timeout=self.recv_timeout())
             self.assertEqual("Message %d" % i, msg.content.body)
 
         channel.basic_ack(delivery_tag=msg.delivery_tag, multiple=True)
 
         try:
-            extra = queue.get(timeout=1)
+            extra = queue.get(timeout=self.recv_timeout())
             self.fail("Got unexpected 11th message in original queue: " + extra.content.body)
         except Empty: None
 
@@ -350,11 +350,11 @@ class BasicTests(TestBase):
 
         #only 5 messages should have been delivered (i.e. 45 bytes worth):
         for i in range(1, 6):
-            msg = queue.get(timeout=1)
+            msg = queue.get(timeout=self.recv_timeout())
             self.assertEqual("Message %d" % i, msg.content.body)
 
         try:
-            extra = queue.get(timeout=1)
+            extra = queue.get(timeout=self.recv_timeout())
             self.fail("Got unexpected 6th message in original queue: " + extra.content.body)
         except Empty: None
 
@@ -362,13 +362,13 @@ class BasicTests(TestBase):
         channel.basic_ack(delivery_tag=msg.delivery_tag, multiple=True)
 
         for i in range(6, 11):
-            msg = queue.get(timeout=1)
+            msg = queue.get(timeout=self.recv_timeout())
             self.assertEqual("Message %d" % i, msg.content.body)
 
         channel.basic_ack(delivery_tag=msg.delivery_tag, multiple=True)
 
         try:
-            extra = queue.get(timeout=1)
+            extra = queue.get(timeout=self.recv_timeout())
             self.fail("Got unexpected 11th message in original queue: " + extra.content.body)
         except Empty: None
 
@@ -376,7 +376,7 @@ class BasicTests(TestBase):
         large = "abcdefghijklmnopqrstuvwxyz"
         large = large + "-" + large;
         channel.basic_publish(routing_key="test-prefetch-size", content=Content(large))
-        msg = queue.get(timeout=1)
+        msg = queue.get(timeout=self.recv_timeout())
         self.assertEqual(large, msg.content.body)
 
     def test_get(self):

qpid_tests/broker_0_8/broker.py
@@ -37,7 +37,7 @@ class BrokerTests(TestBase):
         ctag = ch.basic_consume(queue = "myqueue", no_ack = True).consumer_tag
         body = "test no-ack"
         ch.basic_publish(routing_key = "myqueue", content = Content(body))
-        msg = self.client.queue(ctag).get(timeout = 5)
+        msg = self.client.queue(ctag).get(timeout=self.recv_timeout())
         self.assert_(msg.content.body == body)
 
         # Acknowledging consumer
@@ -45,7 +45,7 @@ class BrokerTests(TestBase):
         ctag = ch.basic_consume(queue = "otherqueue", no_ack = False).consumer_tag
         body = "test ack"
         ch.basic_publish(routing_key = "otherqueue", content = Content(body))
-        msg = self.client.queue(ctag).get(timeout = 5)
+        msg = self.client.queue(ctag).get(timeout=self.recv_timeout())
         ch.basic_ack(delivery_tag = msg.delivery_tag)
         self.assert_(msg.content.body == body)
         
@@ -62,7 +62,7 @@ class BrokerTests(TestBase):
 
         body = "Immediate Delivery"
         channel.basic_publish(exchange="test-exchange", routing_key="key", content=Content(body), immediate=True)
-        msg = queue.get(timeout=5)
+        msg = queue.get(timeout=self.recv_timeout())
         self.assert_(msg.content.body == body)
 
         # TODO: Ensure we fail if immediate=True and there's no consumer.
@@ -81,7 +81,7 @@ class BrokerTests(TestBase):
         channel.basic_publish(exchange="test-exchange", routing_key="key", content=Content(body))
         reply = channel.basic_consume(queue="test-queue", no_ack=True)
         queue = self.client.queue(reply.consumer_tag)
-        msg = queue.get(timeout=5)
+        msg = queue.get(timeout=self.recv_timeout())
         self.assert_(msg.content.body == body)
 
     def test_invalid_channel(self):
@@ -111,10 +111,10 @@ class BrokerTests(TestBase):
         channel.channel_flow(active=False)        
         channel.basic_publish(routing_key="flow_test_queue", content=Content("abcdefghijklmnopqrstuvwxyz"))
         try:
-            incoming.get(timeout=1) 
+            incoming.get(timeout=self.recv_timeout_negative())
             self.fail("Received message when flow turned off.")
         except Empty: None
         
         channel.channel_flow(active=True)
-        msg = incoming.get(timeout=1)
+        msg = incoming.get(timeout=self.recv_timeout())
         self.assertEqual("abcdefghijklmnopqrstuvwxyz", msg.content.body)

qpid_tests/broker_0_8/example.py
@@ -84,7 +84,7 @@ class ExampleTest (TestBase):
         # Now we'll wait for the message to arrive. We can use the timeout
         # argument in case the server hangs. By default queue.get() will wait
         # until a message arrives or the connection to the server dies.
-        msg = queue.get(timeout=10)
+        msg = queue.get(timeout=self.recv_timeout())
 
         # And check that we got the right response with assertEqual
         self.assertEqual(body, msg.content.body)

qpid_tests/broker_0_8/queue.py
@@ -51,7 +51,7 @@ class QueueTests(TestBase):
         channel.basic_publish(exchange="test-exchange", routing_key="key", content=Content("four"))
         reply = channel.basic_consume(queue="test-queue", no_ack=True)
         queue = self.client.queue(reply.consumer_tag)
-        msg = queue.get(timeout=1)
+        msg = queue.get(timeout=self.recv_timeout())
         self.assertEqual("four", msg.content.body)
 
         #check error conditions (use new channels): 
@@ -207,7 +207,7 @@ class QueueTests(TestBase):
         #empty queue:
         reply = channel.basic_consume(queue="delete-me-2", no_ack=True)
         queue = self.client.queue(reply.consumer_tag)
-        msg = queue.get(timeout=1)
+        msg = queue.get(timeout=self.recv_timeout())
         self.assertEqual("message", msg.content.body)
         channel.basic_cancel(consumer_tag=reply.consumer_tag)
 

qpid_tests/broker_0_8/tx.py
@@ -36,18 +36,18 @@ class TxTests(TestBase):
 
         #check results
         for i in range(1, 5):
-            msg = queue_c.get(timeout=1)
+            msg = queue_c.get(timeout=self.recv_timeout())
             self.assertEqual("TxMessage %d" % i, msg.content.body)
 
-        msg = queue_b.get(timeout=1)
+        msg = queue_b.get(timeout=self.recv_timeout())
         self.assertEqual("TxMessage 6", msg.content.body)
 
-        msg = queue_a.get(timeout=1)
+        msg = queue_a.get(timeout=self.recv_timeout())
         self.assertEqual("TxMessage 7", msg.content.body)
 
         for q in [queue_a, queue_b, queue_c]:
             try:
-                extra = q.get(timeout=1)
+                extra = q.get(timeout=self.recv_timeout_negative())
                 self.fail("Got unexpected message: " + extra.content.body)
             except Empty: None
 
@@ -64,7 +64,7 @@ class TxTests(TestBase):
 
         for q in [queue_a, queue_b, queue_c]:
             try:
-                extra = q.get(timeout=1)
+                extra = q.get(timeout=self.recv_timeout_negative())
                 self.fail("Got unexpected message: " + extra.content.body)
             except Empty: None
 
@@ -72,18 +72,18 @@ class TxTests(TestBase):
 
         #check results
         for i in range(1, 5):
-            msg = queue_a.get(timeout=1)
+            msg = queue_a.get(timeout=self.recv_timeout())
             self.assertEqual("Message %d" % i, msg.content.body)
 
-        msg = queue_b.get(timeout=1)
+        msg = queue_b.get(timeout=self.recv_timeout())
         self.assertEqual("Message 6", msg.content.body)
 
-        msg = queue_c.get(timeout=1)
+        msg = queue_c.get(timeout=self.recv_timeout())
         self.assertEqual("Message 7", msg.content.body)
 
         for q in [queue_a, queue_b, queue_c]:
             try:
-                extra = q.get(timeout=1)
+                extra = q.get(timeout=self.recv_timeout_negative())
                 self.fail("Got unexpected message: " + extra.content.body)
             except Empty: None
 
@@ -100,7 +100,7 @@ class TxTests(TestBase):
 
         for q in [queue_a, queue_b, queue_c]:
             try:
-                extra = q.get(timeout=1)
+                extra = q.get(timeout=self.recv_timeout_negative())
                 self.fail("Got unexpected message: " + extra.content.body)
             except Empty: None
 
@@ -108,18 +108,18 @@ class TxTests(TestBase):
 
         #check results
         for i in range(1, 5):
-            msg = queue_a.get(timeout=1)
+            msg = queue_a.get(timeout=self.recv_timeout())
             self.assertEqual("Message %d" % i, msg.content.body)
 
-        msg = queue_b.get(timeout=1)
+        msg = queue_b.get(timeout=self.recv_timeout())
         self.assertEqual("Message 6", msg.content.body)
 
-        msg = queue_c.get(timeout=1)
+        msg = queue_c.get(timeout=self.recv_timeout())
         self.assertEqual("Message 7", msg.content.body)
 
         for q in [queue_a, queue_b, queue_c]:
             try:
-                extra = q.get(timeout=1)
+                extra = q.get(timeout=self.recv_timeout_negative())
                 self.fail("Got unexpected message: " + extra.content.body)
             except Empty: None
 
@@ -155,19 +155,19 @@ class TxTests(TestBase):
         sub_a = channel.basic_consume(queue=name_a, no_ack=False)
         queue_a = self.client.queue(sub_a.consumer_tag)
         for i in range(1, 5):
-            msg = queue_a.get(timeout=1)
+            msg = queue_a.get(timeout=self.recv_timeout())
             self.assertEqual("Message %d" % i, msg.content.body)
         channel.basic_ack(delivery_tag=msg.delivery_tag, multiple=True)    
 
         sub_b = channel.basic_consume(queue=name_b, no_ack=False)
         queue_b = self.client.queue(sub_b.consumer_tag)
-        msg = queue_b.get(timeout=1)
+        msg = queue_b.get(timeout=self.recv_timeout())
         self.assertEqual("Message 6", msg.content.body)
         channel.basic_ack(delivery_tag=msg.delivery_tag)    
 
         sub_c = channel.basic_consume(queue=name_c, no_ack=False)
         queue_c = self.client.queue(sub_c.consumer_tag)
-        msg = queue_c.get(timeout=1)
+        msg = queue_c.get(timeout=self.recv_timeout())
         self.assertEqual("Message 7", msg.content.body)
         channel.basic_ack(delivery_tag=msg.delivery_tag)    
 
@@ -195,7 +195,7 @@ class TxTests(TestBase):
         sub = channel.basic_consume(queue="commit-overlapping", no_ack=False)
         queue = self.client.queue(sub.consumer_tag)
         for i in range(1, 10):
-            msg = queue.get(timeout=1)
+            msg = queue.get(timeout=self.recv_timeout())
             self.assertEqual("Message %d" % i, msg.content.body)
             if i in [3, 6, 10]:
                 channel.basic_ack(delivery_tag=msg.delivery_tag)    
@@ -204,6 +204,6 @@ class TxTests(TestBase):
 
         #check all have been acked:
         try:
-            extra = queue.get(timeout=1)
+            extra = queue.get(timeout=self.recv_timeout_negative())
             self.fail("Got unexpected message: " + extra.content.body)
         except Empty: None

qpid_tests/broker_0_9/echo.py
@@ -58,7 +58,7 @@ class EchoTests(TestBase):
             content=Content(body),
             routing_key="q")
     channel.tx_commit()
-    msg = consumer.get(timeout=1)
+    msg = consumer.get(timeout=self.recv_timeout())
     channel.basic_ack(delivery_tag=msg.delivery_tag)
     channel.tx_commit()
     self.assertEqual(len(body), len(msg.content.body))
@@ -85,9 +85,11 @@ class EchoTests(TestBase):
     try:
       # Create a second connection with minimum framesize.  The Broker will then be forced to chunk
       # the content in order to send it to us.
-      consuming_client = qpid.client.Client(self.config.broker.host, self.config.broker.port)
+      consuming_client = qpid.client.Client(self.config.broker.host, self.config.broker.port or self.DEFAULT_PORT)
       tune_params = { "frame_max" : self.client.conn.FRAME_MIN_SIZE }
-      consuming_client.start(username = self.config.broker.user, password = self.config.broker.password, tune_params = tune_params)
+      consuming_client.start(username = self.config.broker.user or self.DEFAULT_USERNAME,
+                             password = self.config.broker.password or self.DEFAULT_PASSWORD,
+                             tune_params = tune_params)
 
       consuming_channel = consuming_client.channel(1)
       consuming_channel.channel_open()
@@ -95,7 +97,7 @@ class EchoTests(TestBase):
 
       consumer_reply = consuming_channel.basic_consume(queue=queue_name, no_ack=False)
       consumer = consuming_client.queue(consumer_reply.consumer_tag)
-      msg = consumer.get(timeout=1)
+      msg = consumer.get(timeout=self.recv_timeout())
       consuming_channel.basic_ack(delivery_tag=msg.delivery_tag)
       consuming_channel.tx_commit()
 
@@ -135,7 +137,7 @@ class EchoTests(TestBase):
       consumer = self.consume("q", no_ack=False)
 
       # Get and ack/commit the first message
-      msg = consumer.get(timeout=1)
+      msg = consumer.get(timeout=self.recv_timeout())
       channel.basic_ack(delivery_tag=msg.delivery_tag)
       channel.tx_commit()
       # In the problematic case, the Broker interleaves our commit-ok response amongst the content
@@ -147,7 +149,7 @@ class EchoTests(TestBase):
       self.assertEqual(expectedBody, msg.content.body)
 
       for i in range(1, len(bodies)):
-        msg = consumer.get(timeout=5)
+        msg = consumer.get(timeout=self.recv_timeout())
 
         expectedBody = bodies[i]
         self.assertEqual(len(expectedBody), len(msg.content.body))

commit f0217e0c6e40d4b8b60e0a3ae9d7516a79503708

QPID-7588: [Python Test Suite] Fix interpret-time problem introduced by 7e0e81b17511

qpid/testlib.py
@@ -49,7 +49,6 @@ class TestBase(unittest.TestCase):
     DEFAULT_USERNAME = "guest"
     DEFAULT_PASSWORD = "guest"
     DEFAULT_PORT = 5672
-    DEFAULT_PORT_TLS = 5671
 
     def configure(self, config):
         self.config = config
@@ -194,6 +193,10 @@ class TestBase010(unittest.TestCase):
     """
     Base class for Qpid test cases. using the final 0-10 spec
     """
+    DEFAULT_USERNAME = "guest"
+    DEFAULT_PASSWORD = "guest"
+    DEFAULT_PORT = 5672
+    DEFAULT_PORT_TLS = 5671
 
     def configure(self, config):
         self.config = config