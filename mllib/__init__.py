0c7e96af4e540df26372f8e4dd2f7f1b0a997ffd
Justin Ross <jross@apache.org>
2017-03-11 12:51:38 -0800

    QPID-2524: Always use the file:// prefix

--- mllib/__init__.py
+++ mllib/__init__.py
@@ -66,12 +66,7 @@ class Resolver:
     return InputSource(systemId)
 
 def xml_parse(filename, path=()):
-  if sys.version_info[0:2] == (2,3):
-    # XXX: this is for older versions of python
-    from urllib import pathname2url
-    source = "file:%s" % pathname2url( os.path.abspath( filename ) )
-  else:
-    source = filename
+  source = "file://%s" % os.path.abspath(filename)
   h = parsers.XMLParser()
   p = xml.sax.make_parser()
   p.setContentHandler(h)
